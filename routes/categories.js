f7aba32ca09a0aec3448773ca7a8eed7f8bcf42b
blesssingskelvinsaka@gmail.com <blessingskelvinsaka@gmail.com>
2023-12-29 11:03:48 +0200

    setup API for category & products

--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,87 @@
-const {Category} = require ('../models/category')
+
+const { Category } = require('../models/category');
 const express = require('express');
 const router = express.Router();
 
-router.get(`/`, async (req, res) => {
-    const CategoryList = await Category.find();
+router.get('/', async (req, res) => {
+    try {
+        const categoryList = await Category.find();
 
-    if(!CategoryList) {
-        res.status(500).json({success: false})
+        if (!categoryList) {
+            return res.status(500).json({ success: false });
+        }
+        
+        res.status(200).send(categoryList);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: error.message });
     }
-    res.send(CategoryList);
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id);
+
+        if (!category) {
+            return res.status(404).json({ success: false, message: 'The category with the given id was not found' });
+        }
+
+        res.status(200).send(category);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        let category = new Category({
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color,
+        });
+
+        category = await category.save();
+
+        if (!category) {
+            return res.status(484).send('The category cannot be created');
+        }
+
+        res.send(category);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
+router.put('/:id', async (req, res)=>{
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name:req.body.name,
+            icon:req.body.icon,
+            color:req.body.name
+        }
+    )
+     if (!category) {
+            return res.status(484).send('The category cannot be created');
+        }
+    
 })
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const category = await Category.findByIdAndDelete(req.params.id);
+
+        if (category) {
+            return res.status(200).json({ success: true, message: 'The category is deleted' });
+        } else {
+            return res.status(404).json({ success: false, message: 'The category not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ success: false, error: error.message });
+    }
+});
+
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,19 @@
 const {Product} = require('../models/product');
+const { Category } = require('../models/category');
 const express = require ('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 
 
 router.get(`/`, async (req, res) =>{
-    const productList = await Product.find();
+
+    let filter = {};
+    if (req.query.categories)
+    {
+        filter = {category: req.query.categories.split(',')}
+    }
+    const productList = await Product.find(filter).populate('category');
  
     if (!productList){
        res.status(500).json({sucess:false})
@@ -13,23 +21,122 @@ router.get(`/`, async (req, res) =>{
     res.send(productList);
  } )
 
-router.post(`/`, (req, res) =>{
-    const product = new Product({
-       name: req.body.name,
-       image: req.body.image,
-       countInStock: req.body.countInStock
+ router.get(`/:id`, async (req, res) =>{
+   const product = await Product.findById(req.params.id);
+
+   if (!product){
+      res.status(500).json({sucess:false})
+   }
+   res.send(product);
+} )
+
+router.post(`/`, async (req, res) =>{
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('invalid category ')
+
+    let product = new Product({
+       name:req.body.name,
+       description:req.body.description,
+       richDescription:req.body.richDescription,
+       image:req.body.image,
+       brand:req.body.brand,
+       price:req.body.price,
+       category:req.body.category,
+       countInStock:req.body.countInStock,
+       rating:req.body.rating,
+       numReviews:req.body.numReviews,
+       isFeatured:req.body.isFeatured,
  
     })
  
-     product.save().then((createdProduct=> {
-       res.status(201).json(createdProduct)
-     })).catch((err)=> {
-          res.status(500).json({
-             error : err,
-             sucess : false
-          })
- 
-     })
- 
+    product = await product.save();
+   
+    if(!product)
+    return res.status(500).send('the product cannot be created ')
+
+    res.send(product);
   })
+
+  router.put('/:id', async (req, res)=>{
+   if(!mongoose.isValidObjectId(req.params.id)){
+    res.status(400).send('invalid product id ')
+   }
+   const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('invalid category ')
+
+   const product= await Product.findByIdAndUpdate(
+       req.params.id,
+       {
+         name:req.body.name,
+         description:req.body.description,
+         richDescription:req.body.richDescription,
+         image:req.body.image,
+         brand:req.body.brand,
+         price:req.body.price,
+         category:req.body.category,
+         countInStock:req.body.countInStock,
+         rating:req.body.rating,
+         numReviews:req.body.numReviews,
+         isFeatured:req.body.isFeatured,
+      
+      },
+      {new: true}
+
+   )
+    if (!product) {
+           return res.status(484).send('The product cannot be created');
+       }
+   
+})
+router.delete('/:id', async (req, res) => {
+   try {
+       const product = await Product.findByIdAndDelete(req.params.id);
+
+       if (product) {
+           return res.status(200).json({ success: true, message: 'The product is deleted' });
+       } else {
+           return res.status(404).json({ success: false, message: 'The product not found' });
+       }
+   } catch (error) {
+       console.error(error);
+       return res.status(400).json({ success: false, error: error.message });
+   }
+})
+
+router.get(`/get/count`, async (req, res) => {
+    try {
+        const productCount = await Product.countDocuments();
+        
+        if (!productCount) {
+            res.status(500).json({ success: false });
+        }
+
+        res.send({
+            productCount: productCount
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: error.message });
+    }
+})
+router.get(`/get/featured/:count`, async (req, res) => {
+    try {
+        const count = req.params.count ? req.params.count: 0
+        const products = await Product.find({isFeatured:true}).limit(+count)
+        
+        if (!products) {
+            res.status(500).json({ success: false });
+        }
+
+        res.send(products);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
+
+
+
   module.exports = router;
